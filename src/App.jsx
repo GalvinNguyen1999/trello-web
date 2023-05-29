e49cbc9f6ce3b9a44003f61535a05bde50f3516b
DinhCuong <nguyendinhcuongyp@gmail.com>
2023-05-29 21:47:48 +0700

    Dark light p1

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>Dinh Cuong Dev</div>
       <Typography variant='body2' color='text.secondary'>Test Typo</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-
       <br />
       <HomeIcon />
       <HomeIcon color="primary" />
