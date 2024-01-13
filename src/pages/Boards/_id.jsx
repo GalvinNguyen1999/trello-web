0e358d4b794f4e510f74490987efc0a7e5987b6a
Cuong Nguyen <galvinnguyen1999@gmail.com>
2024-01-13 16:32:03 +0700

    Update Create Column

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,6 +3,8 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
+import { generatePlaceholderCard } from '~/utils/formatters'
+import { isEmpty } from 'lodash'
 
 // import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis/index'
@@ -13,6 +15,13 @@ const Board = () => {
   useEffect(() => {
     const boardId = '65a215706b8948401c354667'
     fetchBoardDetailsAPI(boardId).then(board => {
+      board.columns.forEach(column => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
+
       setBoard(board)
     })
   }, [])
@@ -23,16 +32,30 @@ const Board = () => {
       ...newColumnData,
       boardId: board._id
     })
+
+    newColumn.cards = [generatePlaceholderCard(newColumn)]
+    newColumn.cardOrderIds = [generatePlaceholderCard(newColumn)._id]
+
+    const newBoard = { ...board }
+    newBoard.columns.push(newColumn)
+    newBoard.columnOrderIds.push(newColumn._id)
+    setBoard(newBoard)
   }
 
   /* API Cards */
   const createNewCard = async (newCardData) => {
-    const cardData = {
+    const newCard = await createNewCardAPI({
       ...newCardData,
       boardId: board._id
+    })
+
+    const newBoard = { ...board }
+    const updateColumn = newBoard.columns.find(column => column._id === newCard.columnId)
+    if (updateColumn) {
+      updateColumn.cards.push(newCard)
+      updateColumn.cardOrderIds.push(newCard._id)
     }
-    const newCard = await createNewCardAPI(cardData)
-    return newCard
+    setBoard(newBoard)
   }
 
   return (
